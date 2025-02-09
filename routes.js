55e48db9b70ad899d86ef571fb8dcf86d6e5756d
Fedor Borshev <fedor@borshev.com>
2025-02-09 19:40:14 +0300

    Removed language from the URL

--- routes.js
+++ routes.js
@@ -4,32 +4,24 @@ const api = require("./lib/api");
 const router = express.Router();
 
 router.get("/:slug", async (req, res) => {
-  const diploma = await api.fetchDiploma({ slug: req.params.slug });
-  res.redirect(301, `/${diploma.slug}/${diploma.language.toLowerCase()}`);
+  const { slug } = req.params;
+  const diploma = await api.fetchDiploma({ slug });
+
+  const languages = {};
+  languages[diploma.language.toLowerCase()] = { slug: diploma.slug };
+  diploma.other_languages.forEach((diplomaInOtherLanguage) => {
+    languages[diplomaInOtherLanguage.language.toLowerCase()] = {
+      slug: diplomaInOtherLanguage.slug,
+    };
+  });
+  res.render("diploma.html", { diploma, languages });
 });
 
-router.get("/:slug/:language", async (req, res) => {
+router.get("/:slug/:language", async (req, res) => { // legacy route
   const { slug, language } = req.params;
   const diploma = await api.fetchDiploma({ slug });
 
-  if (diploma.language.toLowerCase() !== language.toLowerCase()) {
-    const diplomaToRedirect = diploma.other_languages.find(
-      (d) => d.language === language.toUpperCase(),
-    );
-    res.redirect(
-      301,
-      `/${diplomaToRedirect.slug}/${diplomaToRedirect.language.toLowerCase()}`,
-    );
-  } else {
-    const languages = {};
-    languages[diploma.language.toLowerCase()] = { slug: diploma.slug };
-    diploma.other_languages.forEach((diplomaInOtherLanguage) => {
-      languages[diplomaInOtherLanguage.language.toLowerCase()] = {
-        slug: diplomaInOtherLanguage.slug,
-      };
-    });
-    res.render("diploma.html", { diploma, languages });
-  }
+  res.redirect(`/${diploma.slug}`);
 });
 
 module.exports = router;
